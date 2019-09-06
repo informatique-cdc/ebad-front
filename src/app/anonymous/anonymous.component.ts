37c1743be1de2e36e50a46f3970f4e62677f57bb
Damien Trouillet <damien.trouillet@caissedesdepots.fr>
2019-09-06 08:48:55 +0200

    add anonymous page

---
+++ src/app/anonymous/anonymous.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-anonymous',
+  templateUrl: './anonymous.component.html',
+  styleUrls: ['./anonymous.component.scss']
+})
+export class AnonymousComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
