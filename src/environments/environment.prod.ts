99e051de94f2b15551f9f15b145a77a08ca0c958
Damien Trouillet <contact@damien-trouillet.fr>
2020-01-08 01:57:07 +0100

    use secure protocol

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://ebad.herokuapp.com/ebad',
+  apiUrl: 'https://ebad.herokuapp.com/ebad',
   jwt: true,
   atHash: 'at_hash',
   issuer: '',
