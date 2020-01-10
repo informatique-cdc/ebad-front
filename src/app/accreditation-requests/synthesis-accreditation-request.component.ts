65699a686479b5d0ce22dc7918311d805a91295a
Damien Trouillet <damien.trouillet@caissedesdepots.fr>
2020-01-10 16:02:14 +0100

    remove notifier

--- src/app/accreditation-requests/synthesis-accreditation-request.component.ts
+++ src/app/accreditation-requests/synthesis-accreditation-request.component.ts
@@ -1,10 +1,4 @@
-import {Component, OnInit} from '@angular/core';
-import {AccreditationRequestsService, ApplicationsService} from '../core/services';
-import {Observable, of} from "rxjs";
-import {catchError, debounceTime, distinctUntilChanged, map, switchMap, tap} from "rxjs/operators";
-import {Application, CreationAccreditationRequest} from "../core/models";
-import {Pageable} from "../core/models/pageable.model";
-import {NotifierService} from "angular-notifier";
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'ebad-synthesis-accreditation-request',
