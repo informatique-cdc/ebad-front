17a81ef5be94cd0475821d6b0347ef0d81d8fc0d
Damien Trouillet <contact@damien-trouillet.fr>
2021-06-13 02:19:31 +0200

    update server

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const app = express();
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 app.use(express.static(__dirname + '/dist/ebad-front'));
+app.use(cors(
+  {
+    credentials: true,
+    origin: function (origin, callback) {
+      callback(null, true)
+    }
+  }
+  )
+);
+var apiProxy = createProxyMiddleware('/ebad', {target: 'https://ebad.herokuapp.com', changeOrigin: true,  headers: {
+    Origin: 'http://localhost:4200'
+  }});
+app.use('/ebad', apiProxy);
 
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/ebad-front/index.html'));
