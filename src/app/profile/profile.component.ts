348fbf6c3d014572b5c2518d25a82d48d3f18101
Damien Trouillet <damien.trouillet@caissedesdepots.fr>
2021-06-30 16:19:21 +0200

    Add translation choice and user dropdown

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,11 +1,20 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {User, UserService} from '../core';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: []
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit{
+  user: User;
 
+  constructor(private userService: UserService) {
+  }
+
+  ngOnInit(): void {
+    this.user = this.userService.getCurrentUser();
+    console.log(this.user);
+  }
 }
 
