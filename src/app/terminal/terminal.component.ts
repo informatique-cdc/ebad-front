7e15a3d256bf404b3bcdd292da811513b884b8b8
Damien Trouillet <damien.trouillet@caissedesdepots.fr>
2021-12-30 17:21:30 +0100

    Fix unit test

--- src/app/terminal/terminal.component.ts
+++ src/app/terminal/terminal.component.ts
@@ -23,26 +23,11 @@ export class TerminalComponent implements AfterViewInit, OnDestroy {
         this.terminalSubscription = this.rxStompService.watch('/user/queue/terminal1').subscribe({
             next: result => {
                 this.child.write(result.body);
-                console.log(result);
             }
         });
 
         this.child.keyEventInput.subscribe(e => {
-            console.log('keyboard event:' + e.domEvent.keyCode + ', ' + e.key);
             this.rxStompService.publish({destination: '/ebad/terminal2', body: e.key});
-
-            const ev = e.domEvent;
-            const printable = !ev.altKey && !ev.ctrlKey && !ev.metaKey;
-
-            if (ev.keyCode === 13) {
-               // this.child.write('\n' + FunctionsUsingCSI.cursorColumn(1) + '$ '); // \r\n
-            } else if (ev.keyCode === 8) {
-                if (this.child.underlying.buffer.active.cursorX > 2) {
-                 //   this.child.write('\b \b');
-                }
-            } else if (printable) {
-                //this.child.write(e.key);
-            }
         });
     }
 
